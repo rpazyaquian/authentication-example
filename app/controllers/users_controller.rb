dbe68500ac11c247ccbff18c12627b5fcbc1748e
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-10 11:43:21 -0400

    created Users controller, index view, trying to figure out sessions and stuff now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @login_user = User.new
+    session[:user_id] = 1
+    @logged_in_user = User.find(session[:user_id]) if session[:user_id]
   end
 end
